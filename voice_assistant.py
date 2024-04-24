b35e1a7f567f497c17b21b8ada410545f8ad6467
arungeorgesaji <109901706+arungeorgesaji@users.noreply.github.com>
2024-04-24 14:16:09 +0530

    Add files via upload

--- voice_assistant.py
+++ voice_assistant.py
@@ -30,14 +30,23 @@ login("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")
 gemma7b = HuggingFaceHub(repo_id='google/gemma-1.1-7b-it')
 
 def gemma7b_response(input_text,context,length):
+    search_results = search_google(input_text)
+    location = get_location()
+    city = location['city']
+    country = location['country_name']
+    local_time = get_time()
     template = f"""
     ###context:{context},
-    ###instruction:Please provide your response based solely on the information provided in the context and provide the complete answer.
+    ###instruction:Provide the complete answer,also you can use the search results which I have got for the same query but I cannot confirm its related please carefully access if its need and if its needed you can assist in providing the response to the user.Also make sure to use proper grammer and other characters as this is being read by a text to speech program also you are allowed to your own answer its not necessary you use the google results but for information like time and stuff which changes like you might not exactly know of right now please refer the search results and using that please provide trhe approprita answer in a human friendly way.Also you can use all the other informatioh provided if necessary.Please dont use unnecessary information and just respond with the information the user asked for 
+    ###Search Results from Google for the Same Query: {search_results}
+    ###Users country of origin : {country}
+    ###Users city of origin : {city}
+    ###Users local time: {local_time}
     ###length: {length}
     ###question:{input_text},
     ###answer:
 """
-    response = gemma7b(template).split("###answer:")[1]
+    response = gemma7b(template).split("###answer:")[1].split("**Answer:**")[0]
     return response
 
 def takecommand():
@@ -58,26 +67,6 @@ def takecommand():
         query = 'error'
     return query
 
-def classify_action(input_text):
-    context = """
-I want you to respond with the corresponding number just the number is needed,nothing else should be in the response
-basically i will give a list of like questions give me the most similar question
-
-List of Questions
-What  is the time right now:1
-What is the temperature right now:2
-What is the weather right now:3
-What is the news/current happenings:4
-Play me a song from spotify:5
-Play me a song from youtube:6
-End of list of questions
-
-If the questions doesnt like mean anything similar to all these questions then respond with Never answer with like a sentence only use the corresponding numbers
-and any weird question not similar to any of thhese just answer 0
-"""
-    length="short"
-    return gemma7b_response(input_text,context,length)
-
 def get_response(input_text):
     context = """
 The guy who is asking the question is a blind person who is using you inside the product Nanban
@@ -88,11 +77,12 @@ if needed the numbers and symbols should be expressed in word form.
 """
     length = "as long as possible"
     response = gemma7b_response(input_text,context,length)
+    print(response)
     clean_response = remove_unwanted_characters(response)
     speak(clean_response)
     
 def remove_unwanted_characters(input_text):
-    characters_to_remove = ["*",".","-","_",",",":","?","/",">","<","=","^","(",")","{","}","[","]"]
+    characters_to_remove = ["*","_",":","?","/",">","<","=","^","(",")","{","}","[","]"]
 
     for char in characters_to_remove:
         input_text = input_text.replace(char, "")
@@ -106,50 +96,29 @@ def speak(text, lang='en', output_file='speech.mp3'):
 client_id = 'b13fb24bbdc042e89f761a407c5fa189'
 client_secret = '2a72ff62b0364b1ea881978e62ea7abe'
 
-client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-
-def spotify():
-    query=""
-    #Ask the user which song they want to hear
-    speak("please confirm the song you want to hear")
-    track = takecommand().lower()
-        
-    # Search for the song
-    results = sp.search(q=track, limit=1)
-
-    # Extract track details
-    track = results['tracks']['items'][0]
-    track_name = track['name']
-    track_artist = track['artists'][0]['name']
-    track_preview_url = track['preview_url']
-
-    print(f"Now playing: {track_name} by {track_artist}")
-
-    # If song has no preview URL, exit
-    if track_preview_url is None:
-        print("Sorry, the song cannot be played.")
-        return
-
-    song_content = AudioSegment.from_file(io.BytesIO(requests.get(track_preview_url).content))
-    play(song_content)
-
 def search_song():
-    video=""
-    speak("please confirm the video you want to hear")
-    video = takecommand().lower()
-        
-    results = YoutubeSearch(video, max_results=1).to_dict()
-    if results:
-        video_id = results[0]['id']
-        url = f"https://www.youtube.com/watch?v={video_id}"
-        speak("Downloading  video...")
-        download_audio(url)
-        speak("Playing video...")
-        play_song()
+    while True:
+        video=""
+        speak("please confirm the song you want to hear or use exit to exit")
+        video = takecommand().lower()
+    
+        if video=="error":
+            continue
+        elif video=="exit":
+            speak("Exiting music mode")
+            break
+
+        results = YoutubeSearch(video, max_results=1).to_dict()
+        if results:
+            video_id = results[0]['id']
+            url = f"https://www.youtube.com/watch?v={video_id}"
+            speak("Downloading  song from youtube...")
+            download_audio(url)
+            speak("Playing song...")
+            play_song()
             
-    else:
-        speak("video not found")
+        else:
+           speak("song not found")
 
 def download_audio(url):
     yt = YouTube(url)
@@ -160,54 +129,33 @@ def play_song():
     song = AudioSegment.from_file("temp_song")
     play(song)
 
-def latestnews():
-    api_dict = {
-        "business": "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=68412fb931c1440eab8524ba2128d068",
-        "entertainment": "https://newsapi.org/v2/top-headlines?country=in&category=entertainment&apiKey=68412fb931c1440eab8524ba2128d068",
-        "health": "https://newsapi.org/v2/top-headlines?country=in&category=health&apiKey=68412fb931c1440eab8524ba2128d068",
-        "science": "https://newsapi.org/v2/top-headlines?country=in&category=science&apiKey=68412fb931c1440eab8524ba2128d068",
-        "sports": "https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=68412fb931c1440eab8524ba2128d068",
-        "technology": "https://newsapi.org/v2/top-headlines?country=in&category=technology&apiKey=68412fb931c1440eab8524ba2128d068"
-    }
-
-    content = None
-    url = None
-    speak("Which field news do you want, [business], [health], [technology], [sports], [entertainment], [science]")
-    news_read = False
-    while news_read == False:
-        field = takecommand()
-        for key, value in api_dict.items():
-            if key.lower() in field.lower():
-                url = value
-                news_read = True
-            else:
-                url = True
-        if url is True:
-            pass
-        else:
-            news = requests.get(url).text
-            news = json.loads(news)
-            speak("Here is the first news.")
-
-            arts = news["articles"]
-
-            for articles in arts:
-                article = articles["title"]
-                speak(article)
-
 def get_time():
     strTime = datetime.datetime.now().strftime("%H:%M")
-    speak(f"The current time is {strTime}")
+    return strTime
 
-def get_temperature():
-    location = get_location()
-    city = location['city']
-    search = f"temperature in {city}"
-    url = f"https://www.google.com/search?q={search}"
-    r = requests.get(url)
-    data = BeautifulSoup(r.text, "html.parser")
-    temp_value = data.find("div", class_="BNeawe").text
-    speak(f"current {search} is {temp_value}")
+def search_google(query, num_results=10):
+    """
+    Perform a Google search and return snippets of search results as a string.
+    
+    Args:
+    - query: The search query.
+    - num_results: Number of search results to retrieve (default is 10).
+    
+    Returns:
+    - A string containing snippets of search results.
+    """
+    search_url = f"https://www.google.com/search?q={query}&num={num_results}"
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    response = requests.get(search_url, headers=headers)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    
+    snippets = []
+    for result in soup.find_all('div', class_='tF2Cxc'):
+        snippet = result.find('span', class_='aCOpRe').text
+        snippets.append(snippet)
+    
+    return '\n'.join(snippets)
 
 def get_location():
     ipdata = requests.get(url).json()
@@ -219,19 +167,8 @@ def voice_mode():
     if query=='error':
         return
         
-    action = classify_action(query)
-    
-    if '0' in action:
+    if query=="music mode" or query=="music mod":
+        search_song()                          
+    else:
         get_response(query)
-    elif '1' in action:
-        get_time()
-    elif '2' in action:
-        get_temperature()        
-    elif '3' in action:
-        pass
-    elif '4' in action:
-        latest_news()
-    elif '5' in action:
-        spotify()
-    elif '6' in action:
-       search_song()
+
